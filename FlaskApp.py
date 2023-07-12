744a4263ab88b61be3b1c14a628c0f946b2ba051
Evan Kale <125571305+evan-kale@users.noreply.github.com>
2023-07-12 00:26:29 -0500

    Add files via upload

---
+++ FlaskApp.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, request
+import gspread
+from google.oauth2.service_account import Credentials
+
+#Creating app
+app = Flask(__name__)
+
+#Calling spreadsheets
+spreadsheet_name = "The Bird House Referral Database"
+sheet_name = "Eligibility"
+sheet_name2 = "Waitlist"
+
+#API Credentials
+credentials = Credentials.from_service_account_file('BHEligibility.json')
+scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
+client = gspread.Client(auth=scoped_credentials)
+
+#Opening sheet
+spreadsheet = client.open(spreadsheet_name)
+
+#Naming sheets
+eligibility_sheet = spreadsheet.worksheet(sheet_name)
+waitlist_sheet = spreadsheet.worksheet(sheet_name2)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def eligibility_check():
+    if request.method == 'POST':
+        #Save data from eligibility_check
+        client_first_name = request.form['client_first_name']
+        client_last_name = request.form['client_last_name']
+        birthday = request.form['birthday']
+        life_expectancy = request.form['life_expectancy']
+        enrolled_agency = request.form['enrolled_agency']
+        covid_status = request.form['covid_status']
+        weight = request.form['weight']
+        dnr_status = request.form['dnr_status']
+        attorney_status = request.form['attorney_status']
+        hospice_plan = request.form['hospice_plan']
+        agreement = request.form['agreement']
+        phone = request.form['phone']
+        email = request.form['email']
+
+        #Logic for reason of ineligibility
+        reason = ""
+        #If reason exists, save reason and provide explanation to user
+        if int(life_expectancy) > 6:
+            reason = "Guests are required to have a life expectancy of 6 months or less."
+        elif enrolled_agency.lower() == "n/a":
+            reason = "Guests are required to be enrolled with one of the listed agencies."
+        elif covid_status.lower() != "yes":
+            reason = "Guests must be COVID negative at the time of admission."
+        elif int(weight) > 300:
+            reason = "Guest weight is required to be below 300lbs."
+        elif dnr_status.lower() != "yes":
+            reason = "Guests are required to have a 'Do Not Resuscitate'."
+        elif attorney_status.lower() != "yes":
+            reason = "Guests are required to have a Durable Power of Attorney'."
+        elif hospice_plan.lower() != "yes":
+            reason = "Guests are required to have a hospice plan in case of discharge."
+        elif agreement.lower() != "yes":
+            reason = "Guests and their family/friends are required to agree on the purpose of admission, hospice care plan, and projected length of stay."
+
+        if reason:
+            #Append ineligible users to Eligible sheet
+            eligibility_sheet.append_row([client_first_name, client_last_name, "Ineligible", enrolled_agency, birthday, reason, weight+"lbs", phone, email])
+            #Ineligible message
+            return "The guest is ineligible for The Bird House but may be reconsidered. \nPlease contact (319) 499-1882 or info@hospicehomejc.org if you want additional information."
+
+        #Appending eligible users to Eligible sheet
+        eligibility_sheet.append_row([client_first_name, client_last_name, "Eligible", enrolled_agency, birthday, " ", weight+"lbs", phone, email])
+
+        #Eligible message to user
+        return "Congratulations! The guest is eligible to stay at The Bird House.\nLook for a call from (319) 499-1882 or an email from info@hospicehomejc.org soon!"
+
+    #Returning info to eligibility_check.html
+    return render_template('eligibility_check.html')
+
+
+#Running application locally
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=8123, use_reloader=False)
---
+++ readme.txt
@@ -0,0 +1,13 @@
+﻿Testing Instructions for Local Hosting
+
+
+1. Download BHEligibility.json, FlaskApp.py, and eligibility_check.html into a new folder named “EligibilityCheck”
+2. Create a folder within the newly created folder named “templates” and move eligibility_check.html into it
+3. In your IDE, type “pip install gspread google-auth” and then “pip install flask” into the console. It should take a few seconds to install each package
+4. Run script, console should display locally hosted URL named https://127.0.0.1:8123
+5. Copy link and paste into a browser while script is running
+
+
+
+
+Live site: thebirdhouse.pythonanywhere.com
